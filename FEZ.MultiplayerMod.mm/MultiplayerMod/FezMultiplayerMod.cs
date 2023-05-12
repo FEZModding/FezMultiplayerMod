6c9aa8af8f7e2ccf8fbb48d7cd97d588b8b0bcdd
Jenna Sloan <jenna3715@gmail.com>
2023-05-11 20:26:03 -0500

    well, it draws something

--- FEZ.MultiplayerMod.mm/MultiplayerMod/FezMultiplayerMod.cs
+++ FEZ.MultiplayerMod.mm/MultiplayerMod/FezMultiplayerMod.cs
@@ -19,6 +19,8 @@ using FezEngine.Structure.Input;
 using System.Net.Sockets;
 using System.Text;
 using System.Net;
+using FezEngine;
+using FezEngine.Effects;
 
 namespace FezGame.MultiplayerMod
 {
@@ -48,6 +50,10 @@ namespace FezGame.MultiplayerMod
 
         [ServiceDependency]
         public IFontManager FontManager { private get; set; }
+
+        [ServiceDependency]
+        public ILightingPostProcess LightingPostProcess { private get; set; }
+
         #endregion
 
         public static FezMultiplayerMod Instance;
@@ -60,6 +66,7 @@ namespace FezGame.MultiplayerMod
             Instance = this;
             mp = new MultiplayerClient(game);
             drawer = new SpriteBatch(GraphicsDevice);
+            mesh.AddFace(new Vector3(1f), new Vector3(0f, 0.25f, 0f), FaceOrientation.Front, centeredOnOrigin: true, doublesided: true);
         }
 
         private bool disposing = false;
@@ -78,6 +85,11 @@ namespace FezGame.MultiplayerMod
         public override void Initialize()
         {
             DrawOrder = 4000;
+            LightingPostProcess.DrawGeometryLights += PreDraw;
+            DrawActionScheduler.Schedule(delegate
+            {
+                mesh.Effect = (effect = new GomezEffect());
+            });
         }
         public override void Update(GameTime gameTime)
         {
@@ -88,29 +100,106 @@ namespace FezGame.MultiplayerMod
             }
         }
 
+        private void PreDraw(GameTime gameTime)
+        {
+            if (!GameState.Loading && !PlayerManager.Hidden && !GameState.InFpsMode)
+            {
+                effect.Pass = LightingEffectPass.Pre;
+                if (!PlayerManager.FullBright)
+                {
+                    base.GraphicsDevice.PrepareStencilWrite(StencilMask.Level);
+                }
+                else
+                {
+                    base.GraphicsDevice.PrepareStencilWrite(StencilMask.None);
+                }
+                mesh.Draw();
+                base.GraphicsDevice.PrepareStencilWrite(StencilMask.None);
+                effect.Pass = LightingEffectPass.Main;
+            }
+        }
+
         public override void Draw(GameTime gameTime)
         {
-            if(this.disposing)
+            if (this.disposing)
             {
                 return;
             }
             drawer.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullNone);
             string s = "";
-            foreach(var p in mp.Players.Values)
+            foreach (var p in mp.Players.Values)
             {
-                if(p.uuid == mp.MyUuid)
+                if (p.uuid == mp.MyUuid)
                 {
                     s += "(you): ";
                 }
                 s += $"{p.endpoint}, {p.uuid}, {p.currentLevelName}, {p.action}, {p.position}, {p.lastUpdateTimestamp}\n";
                 //draw other player to screen if in the same level
-                if(p.uuid != mp.MyUuid && p.currentLevelName != null && p.currentLevelName.Length > 0 && p.currentLevelName == LevelManager.Name)
+                if (p.uuid != mp.MyUuid && p.currentLevelName != null && p.currentLevelName.Length > 0 && p.currentLevelName == LevelManager.Name)
                 {
+                    DrawPlayer(p);
                     //TODO actually draw the players on the screen 
                 }
             }
             drawer.DrawString(FontManager.Big, s, Vector2.Zero, Color.Gray, 0f, Vector2.Zero, 2f, SpriteEffects.None, 0f);
             drawer.End();
+        }
+
+        private GomezEffect effect;
+        private Mesh mesh = new Mesh();
+
+        internal void DrawPlayer(MultiplayerClient.PlayerMetadata p)
+        {
+            if (GameState.Loading || GameState.InMap)
+            {
+                return;
+            }
+            AnimatedTexture animation = PlayerManager.GetAnimation(p.action);
+            int width = animation.Texture.Width;
+            int height = animation.Texture.Height;
+            int frame = animation.Timing.Frame;
+            Rectangle rectangle = animation.Offsets[frame];
+            //drawer.Draw(animation.Texture, CameraManager.Position - p.position, Color.White);
+
+            mesh.Texture = animation.Texture;
+            mesh.FirstGroup.TextureMatrix.Set(new Matrix((float)rectangle.Width / (float)width, 0f, 0f, 0f, 0f, (float)rectangle.Height / (float)height, 0f, 0f, (float)rectangle.X / (float)width, (float)rectangle.Y / (float)height, 1f, 0f, 0f, 0f, 0f, 0f));
+            mesh.Scale = new Vector3(animation.FrameWidth / 16f, animation.FrameHeight / 16f, 1f);
+            mesh.Position = p.position;// + GetPositionOffset(PlayerManager);
+            if (!CameraManager.Viewpoint.IsOrthographic() && CameraManager.LastViewpoint != 0)
+            {
+                mesh.Rotation = Quaternion.CreateFromAxisAngle(Vector3.UnitY, CameraManager.LastViewpoint.ToPhi());
+            }
+            else
+            {
+                mesh.Rotation = CameraManager.Rotation;
+            }
+            GraphicsDevice graphicsDevice = base.GraphicsDevice;
+            if (PlayerManager.LookingDirection == HorizontalDirection.Left)
+            {
+                mesh.Rotation *= FezMath.QuaternionFromPhi((float)Math.PI);
+            }
+            if (!p.action.SkipSilhouette())
+            {
+                graphicsDevice.PrepareStencilRead(CompareFunction.Greater, StencilMask.NoSilhouette);
+                mesh.DepthWrites = false;
+                mesh.AlwaysOnTop = true;
+                effect.Silhouette = true;
+                mesh.Draw();
+            }
+            if (!PlayerManager.Background)
+            {
+                graphicsDevice.PrepareStencilRead(CompareFunction.Equal, StencilMask.Hole);
+                mesh.AlwaysOnTop = true;
+                mesh.DepthWrites = false;
+                effect.Silhouette = false;
+                mesh.Draw();
+            }
+            graphicsDevice.PrepareStencilWrite(StencilMask.Gomez);
+            mesh.AlwaysOnTop = p.action.NeedsAlwaysOnTop();
+            mesh.DepthWrites = !GameState.InFpsMode;
+            effect.Silhouette = false;
+            mesh.Draw();
+            graphicsDevice.PrepareStencilWrite(StencilMask.None);
         }
     }
 }
