75d31232b1785d6506c5f68847e2df31f44b4ca8
Jenna Sloan <jenna3715@gmail.com>
2024-12-15 01:21:52 -0600

    Update RichTextRenderer.cs

--- FezMultiplayerMod/RichTextRenderer.cs
+++ FezMultiplayerMod/RichTextRenderer.cs
@@ -37,7 +37,21 @@ namespace FezGame.MultiplayerMod
                 FontData = fontdata;
             }
         }
-        private static IList<FontData> Fonts = new List<FontData>();//TODO populate list
+        private static List<FontData> Fonts = new List<FontData>();//TODO populate list
+        /*
+         * Japanese:
+		 *     Big = CMProvider.Global.Load<SpriteFont>("Fonts/Japanese Big");
+		 *     BigFactor = 0.34125f;
+		 * Korean:
+		 *     Big = CMProvider.Global.Load<SpriteFont>("Fonts/Korean Big");
+		 *     BigFactor = 0.34125f;
+		 * Chinese:
+		 *     Big = CMProvider.Global.Load<SpriteFont>("Fonts/Chinese Big");
+		 *     BigFactor = 0.34125f;
+         * Other:
+		 *     Big = CMProvider.Global.Load<SpriteFont>("Fonts/Latin Big");
+		 *     BigFactor = 2f;
+         */
 
         private static List<TokenizedText> TokenizeChars(string text, FontData defaultFontData, Color defaultColor)
         {
@@ -46,10 +60,12 @@ namespace FezGame.MultiplayerMod
             FontData currentFont = defaultFontData;
             Color currentColor = defaultColor;
             string currentToken = "";
-            foreach (char c in text)
+            for (int i = 0; i < text.Length; ++i)//changed from foreach so we can look ahead from the current position 
             {
+                char c = text[i];
+                //TODO check for special characters to change currentColor and whatever other presentation options we want to include; see "Select Graphic Rendition"
                 currentFont = GetFirstSupportedFont(defaultFontData, c);
-                if(currentFont.Equals(lastFont))
+                if (currentFont.Equals(lastFont))
                 {
                     currentToken += c;
                 }
@@ -62,25 +78,27 @@ namespace FezGame.MultiplayerMod
             }
             return tokens;
         }
+        public Vector2 MeasureString(FontManager fontManager, string text)
+        {
+            return MeasureString(fontManager.Big, fontManager.BigFactor, text);
+        }
         public Vector2 MeasureString(SpriteFont defaultFont, float defaultFontScale, string text)
         {
             FontData defaultFontData = new FontData(defaultFont, defaultFontScale);
             Vector2 size = Vector2.Zero;
             string[] lines = text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
             string line;
-            for (int i=0; i<lines.Length; ++i)
+            for (int i = 0; i < lines.Length; ++i)
             {
                 line = lines[i];
                 Vector2 linesize = Vector2.Zero;
-                void lambda(TokenizedText token)
+                TokenizeChars(line, defaultFontData, Color.White).ForEach((TokenizedText token) =>
                 {
                     FontData fontData = token.FontData;
-                    var tokensize = fontData.Font.MeasureString(token.Text.ToString());
-                    tokensize *= fontData.Scale;
+                    Vector2 tokensize = fontData.Font.MeasureString(token.Text.ToString()) * fontData.Scale;
                     linesize.X += tokensize.X + fontData.Font.Spacing;
                     linesize.Y = Math.Max(linesize.Y, tokensize.Y);
-                }
-                TokenizeChars(line, defaultFontData, Color.White).ForEach(lambda);
+                });
                 size.X = Math.Max(linesize.X, size.X);
                 size.Y += linesize.Y;
                 //check if there's more lines
@@ -91,12 +109,20 @@ namespace FezGame.MultiplayerMod
             }
             return size;
         }
+        public void DrawString(SpriteBatch batch, FontManager fontManager, string text, Vector2 position, Color defaultColor, float scale)
+        {
+            DrawString(batch, fontManager.Big, fontManager.BigFactor, text, position, defaultColor, scale, 0);
+        }
         public void DrawString(SpriteBatch batch, SpriteFont defaultFont, float defaultFontScale, string text, Vector2 position, Color defaultColor, float scale)
         {
             DrawString(batch, defaultFont, defaultFontScale, text, position, defaultColor, scale, 0);
         }
         public void DrawString(SpriteBatch batch, SpriteFont defaultFont, float defaultFontScale, string text, Vector2 position, Color defaultColor, float scale, float layerDepth)
         {
+            /*
+             * Note: currently, I think tokens are drawn with vertical-align: top
+             * Because of this, it is very important that the font scale and values in List<FontData> are correct, and all fonts appear the same scale
+             */
             Vector2 currentPositionOffset = Vector2.Zero;
             FontData defaultFontData = new FontData(defaultFont, defaultFontScale);
             string[] lines = text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
@@ -105,20 +131,20 @@ namespace FezGame.MultiplayerMod
             {
                 line = lines[i];
                 Vector2 linesize = Vector2.Zero;
-                void lambda(TokenizedText token)
+                TokenizeChars(line, defaultFontData, defaultColor).ForEach((TokenizedText token) =>
                 {
                     FontData fontData = token.FontData;
-                    var tokensize = fontData.Font.MeasureString(token.Text.ToString()) *  fontData.Scale;
+                    Vector2 tokensize = fontData.Font.MeasureString(token.Text.ToString()) * fontData.Scale;
+                    batch.DrawString(fontData.Font, token.Text, position + currentPositionOffset, token.Color, 0f, Vector2.Zero, fontData.Scale * scale, SpriteEffects.None, layerDepth);
                     linesize.X += tokensize.X + fontData.Font.Spacing;
                     linesize.Y = Math.Max(linesize.Y, tokensize.Y);
                     currentPositionOffset.X += linesize.X;
-                    batch.DrawString(fontData.Font, token.Text, position + currentPositionOffset, token.Color, 0f, Vector2.Zero, fontData.Scale * scale, SpriteEffects.None, layerDepth);
-                }
-                TokenizeChars(line, defaultFontData, Color.White).ForEach(lambda);
+                });
                 currentPositionOffset.Y += linesize.Y;
                 //check if there's more lines
                 if (i + 1 < lines.Length)
                 {
+                    currentPositionOffset.X = 0;
                     currentPositionOffset.Y += defaultFontData.Font.LineSpacing * defaultFontData.Scale;
                 }
             }
