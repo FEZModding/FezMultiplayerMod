7933c55b0b4172da81c105b40b893890f6d58b01
Jenna Sloan <jenna3715@gmail.com>
2025-01-13 21:08:09 -0600

    observes all in SaveDataObserver

--- FezMultiplayerMod/MultiplayerMod/SaveDataObserver.cs
+++ FezMultiplayerMod/MultiplayerMod/SaveDataObserver.cs
@@ -105,47 +105,122 @@ namespace FezGame.MultiplayerMod
                         changes.Add(containerIdentifier, field, currentVal, oldVal);
                     }
                 }
-                //TODO handle collections like Lists and Dictionaries
+                // handle collections like Lists
                 else if (typeof(IList).IsAssignableFrom(fieldType) || (fieldType.IsGenericType && fieldType.GetGenericTypeDefinition() == typeof(List<>)))
                 {
+                    //Note: all the List fields in SaveData are of value types (namely List<string> and List<ActorType>)
+                    //Note: all the List fields in LevelSaveData are of value types (namely List<TrileEmplacement> and List<int>)
+                    //Note: the only List fields in WinConditions is of value type List<int>
+                    //This means none of the fields in SaveData or the types it contains have a List of non-value types
+
                     Console.Write(fieldType);
-                    //TODO iterate through the entries checking values
-                    //CheckType(changes, typeof(fieldType), containerIdentifier+IDENTIFIER_SEPARATOR+"List TODO", currentVal, oldVal);
+                    HashSet<object> currentVal_hashset = new HashSet<object>(((IList)currentVal).Cast<object>());
+                    HashSet<object> oldVal_hashset = new HashSet<object>(((IList)oldVal).Cast<object>());
+
+                    // Find items in currentVal that are not in oldVal
+                    var addedVals = currentVal_hashset.Except(oldVal_hashset).ToList();
+
+                    // Find items in list2 that are not in list1
+                    var removedVals = oldVal_hashset.Except(currentVal_hashset).ToList();
+
+                    if (addedVals.Count > 0 || removedVals.Count > 0)
+                    {
+                        // add changes to changes
+                        addedVals.ForEach(addedVal =>
+                        {
+                            changes.Add(containerIdentifier, field, addedVal, null);
+                        });
+                        removedVals.ForEach(removedVal =>
+                        {
+                            //TODO? idk if this ever actually happens, but should probably implement it somehow
+                            changes.Add(containerIdentifier, field, null, oldVal);
+                            System.Diagnostics.Debugger.Launch();
+                            System.Diagnostics.Debugger.Break();
+                        });
+                    }
                 }
+                // handle collections like Dictionaries
                 else if (typeof(IDictionary).IsAssignableFrom(fieldType) || (fieldType.IsGenericType && fieldType.GetGenericTypeDefinition() == typeof(Dictionary<,>)))
                 {
+                    //Note: SaveData contains fields of types Dictionary<string, bool> and Dictionary<string, LevelSaveData>
+                    //Note: LevelSaveData contains fields of types Dictionary<int, int>
+
                     Console.Write(fieldType);
-                    //TODO iterate through the entries checking the keys and values
-                    //CheckType(changes, typeof(fieldType), containerIdentifier+IDENTIFIER_SEPARATOR+"Dict TODO", currentVal, oldVal);
+
+                    IDictionary currentVal_dict = (IDictionary)currentVal;
+                    IDictionary oldVal_dict = (IDictionary)oldVal;
+
+                    //iterate through the entries checking the keys and values
+
+                    HashSet<object> currentVal_dictKeys = new HashSet<object>(currentVal_dict.Keys.Cast<object>());
+                    HashSet<object> oldVal_dictKeys = new HashSet<object>(oldVal_dict.Keys.Cast<object>());
+
+                    // Find keys in currentVal_dict that are not in oldVal_dict
+                    var onlyIncurrentVal_dictKeys = currentVal_dictKeys.Except(oldVal_dictKeys).ToList();
+                    var onlyIncurrentVal_dictValues = onlyIncurrentVal_dictKeys.Select(key => currentVal_dict[key]).ToList();
+
+                    // Find keys in oldVal_dict that are not in currentVal_dict
+                    var onlyInoldVal_dictKeys = oldVal_dictKeys.Except(currentVal_dictKeys).ToList();
+                    var onlyInoldVal_dictValues = onlyInoldVal_dictKeys.Select(key => oldVal_dict[key]).ToList();
+
+                    // For keys that are present in both, check for value differences
+                    var sharedKeys = currentVal_dictKeys.Intersect(oldVal_dictKeys).ToList();
+                    foreach (var key in sharedKeys)
+                    {
+                        var value1 = currentVal_dict[key];
+                        var value2 = oldVal_dict[key];
+                        string containerID = containerIdentifier + $"[{key.ToString().Replace("[","%5B").Replace("]","%5D")}]";
+                        void ComparePrimitive()
+                        {
+                            if (!Equals(value1, value2))
+                            {
+                                string msg = $"Key '{key}' has different values: '{value1}' in currentVal_dict and '{value2}' in oldVal_dict";
+                                Console.WriteLine(msg);
+                                System.Diagnostics.Debug.WriteLine(msg);
+                                System.Diagnostics.Debugger.Break();
+                                // add changes to changes
+                                changes.Add(containerID, field, value1, value2);
+                            }
+                        }
+                        if (fieldType.IsGenericType)
+                        {
+                            Type valueType = fieldType.GetGenericArguments()[1];
+                            if (valueType.IsValueType)
+                            {
+                                ComparePrimitive();
+                            }
+                            else
+                            {
+                                // compare values using CheckType
+                                CheckType(changes, valueType, containerID, value1, value2);
+                            }
+                        }
+                        else if (!Equals(value1, value2))
+                        {
+                            ComparePrimitive();
+                        }
+                    }
                 }
+                // handle collections that are neither List nor Dictionary
                 else if (typeof(IEnumerable).IsAssignableFrom(fieldType))
                 {
                     Console.Write(fieldType);
+                    System.Diagnostics.Debug.WriteLine(fieldType);
                     System.Diagnostics.Debugger.Launch();
                     //TODO if this happens, handle the unhandled enumerable type
                 }
                 else
                 {
                     //It must be a non-enumerable object of some kind
-                    //TODO handle non-enumerable object types like LevelSaveData
-                    //CheckType(changes, typeof(fieldType), containerIdentifier+IDENTIFIER_SEPARATOR+ typeof(fieldType).Name + " TODO", currentVal, oldVal);
+                    // handle non-enumerable object types like LevelSaveData and WinConditions
+                    CheckType(changes, fieldType, containerIdentifier + IDENTIFIER_SEPARATOR + fieldType.Name, currentVal, oldVal);
                 }
             }
         }
-        private static bool debuggerWasAttached = false;
         private static void CheckType(SaveDataChanges changes, Type containingType, string containerIdentifier, object currentObject, object oldObject)
         {
             FieldInfo[] fields = containingType.GetFields(BindingFlags.Public | BindingFlags.Instance);
 
-            IDictionary a = new Dictionary<string, bool>();
-
-            #if DEBUG
-            if (!debuggerWasAttached && !System.Diagnostics.Debugger.IsAttached)
-            {
-                System.Diagnostics.Debugger.Launch();
-                debuggerWasAttached = true;
-            }
-            #endif
             foreach (FieldInfo field in fields)
             {
                 CheckField(changes, containerIdentifier, field, currentObject, oldObject);
@@ -153,14 +228,13 @@ namespace FezGame.MultiplayerMod
         }
         public class SaveDataChanges
         {
-            public bool HasChanges { get; private set; } = false;
+            public bool HasChanges => Changes.Any();
 
             //Note this collection should only have a single entry per unique containerIdentifier and field combo
             public readonly Dictionary<string, ChangeInfo> Changes = new Dictionary<string, ChangeInfo>();
 
             internal void Add(string containerIdentifier, FieldInfo field, object currentVal, object oldVal)
             {
-                HasChanges = true;
                 //Note this collection should only have a single entry per unique containerIdentifier and field combo
                 string uniqueIdentifier = containerIdentifier + IDENTIFIER_SEPARATOR + field.Name;
                 if (Changes.TryGetValue(uniqueIdentifier, out ChangeInfo change))
