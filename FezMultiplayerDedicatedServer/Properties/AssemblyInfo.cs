ef79e74ad8a1f572db59d29cc6614702bea6b58d
Jenna Sloan <jenna3715@gmail.com>
2025-01-21 22:14:50 -0600

    changed "ConsoleApp1" to "FezMultiplayerDedicatedServer"

--- FezMultiplayerDedicatedServer/Properties/AssemblyInfo.cs
+++ FezMultiplayerDedicatedServer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("FezMultiplayerDedicatedServer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("FezMultiplayerDedicatedServer")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
