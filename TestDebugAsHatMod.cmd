25be8928932faaa4144df8af2bdb5a3cc7befc62
Jenna Sloan <jenna3715@gmail.com>
2026-02-04 18:06:14 -0600

    update TestDebugAsHatMod.cmd to scan all Steam libs

--- TestDebugAsHatMod.cmd
+++ TestDebugAsHatMod.cmd
@@ -1,21 +1,52 @@
+@echo off
+@setlocal EnableDelayedExpansion
 
 @set projectpath=%cd%
 @set projectpathbin=%cd%\FezMultiplayerMod\bin\Debug
 
 @set fezpath=
 @REM Find the location of the FEZ game folder
-@REM Note: does not check all Steam library locations; see steamapps\libraryfolders.vdf
-@FOR /F "tokens=2* skip=2" %%a in ('reg query "HKCU\SOFTWARE\Valve\Steam" /v "SteamPath"') do @set "fezpath=%%b/steamapps/common/FEZ"
 
-@if not defined fezpath (
-	@set "fezpath=%ProgramFiles(x86)%\Steam\steamapps\common\FEZ"
+@set FezSteamAppId=224760
+@REM Get Steam library folders
+@FOR /F "tokens=2* skip=2" %%a in ('reg query "HKCU\SOFTWARE\Valve\Steam" /v "SteamPath"') do @set "steamlibs=%%b\steamapps\libraryfolders.vdf"
+
+@if defined steamlibs (
+	
+	@set curpath=
+	
+	REM look through steamapps\libraryfolders.vdf to find FEZ (steam app id 224760)
+	@FOR /F "tokens=1*" %%L in ('FINDSTR /r /c:"path" /c:"\<%FezSteamAppId%\>" "%steamlibs%"') do @(
+		@if %%L=="%FezSteamAppId%" (
+			@set fezpath=!curpath!
+		)
+		@if %%L=="path" (
+			@set curpath=%%M
+		)
+	)
+	@REM Note: you must use delayed expansion when reading the values of variables set within an if statement
+	if NOT "!fezpath!" == "" @(
+		set "fezpath=!fezpath:"=!\steamapps\common\FEZ"
+		call :replace_loop
+	)
+)
+@goto done
+:replace_loop
+@set "temp=%fezpath%"
+@set "fezpath=%fezpath:\\=\%"
+@if "%temp%"=="%fezpath%" @goto :eof
+@goto replace_loop
+:done
+
+if not defined fezpath (
+	set "fezpath=%ProgramFiles(x86)%\Steam\steamapps\common\FEZ"
 )
 
 @REM these set statements don't work in IF blocks
 @set "modpath=%fezpath%\Mods\FezMultiplayerMod"
 @REM find MONOMODDED_FEZ
 @set mmfez=MONOMODDED_FEZ.exe
-@FOR /F %%a in ('dir "%fezpath%\MONOMODDED_FEZ*.exe" /B') do set "mmfez=%%a"
+@FOR /F %%a in ('dir "%fezpath%\MONOMODDED_FEZ*.exe" /B') do @set "mmfez=%%a"
 
 echo "%fezpath%"
 echo "%modpath%"
